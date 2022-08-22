fa025514e7245a3385fa67e3c96b06a5e8f321e4
jaewoopk <jaewoopk000@naver.com>
2022-08-22 20:13:51 +0900

    Day 4 Lecture 3 about map

--- Lecture2_test3.java
+++ Lecture2_test3.java
@@ -18,6 +18,31 @@ public class Lecture2_test3 {
             arr[t++] = (int) (n % 10);
             n /= 10;
         }
-        System.out.println(arr[1]);
+        /* 이 풀이 방법이 더 좋은 것 같음
+        *public int[] solution(long n) {
+            int[] answer = {};
+            long value = 0;
+            List<Integer> checkedValues = new ArrayList<>();
+
+            do {
+                value = n % 10;
+                checkedValues.add((int)value);
+                n = n/10;
+            } while(n > 0);
+
+                return listToArray(checkedValues);
+            }
+
+        private int[] listToArray(List<Integer> target) {
+            int targetSize = target.size();
+            int[] intArray = new int[targetSize];
+
+            for (int i = 0; i < targetSize; i++){
+                intArray[i] = target.get(i);
+            }
+
+            return intArray;
+        }
+         */
     }
 }
---
+++ Lecture3_1.java
@@ -0,0 +1,66 @@
+// cannot resolved가 뜰 때는, vscode를 끄고 다시 시작하면 된다.
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/*
+ * Array 장점 + List 장점 => 유연하면서도 빠르게 값을 찾아낼 수 있는
+ * Map : key -> hash function -> hash -> bucket index -> hash collision -> List -> Data
+ * -> dictionay라고도 한다.
+ * 
+ */
+
+ // custom 클래스를 생성한다고 해보자
+class Mydata {
+    int v;
+
+    public Mydata(int v) {
+        this.v = v;
+    }
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return (" [ " + v + " ] ");
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Mydata myData = (Mydata) obj;
+        return v == myData.v;
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return Objects.hash(v);
+    }
+}
+
+public class Lecture3_1 {
+    public static void main(String[] args) {
+        // 유연하면서도 빠르게 읽어내는 자료구조?
+        // 바로 map
+        Map<String, Integer> map = new HashMap<>();
+        map.put("A", 1);
+        map.put("B", 2);
+        map.putIfAbsent("A", 10);
+        map.put("C", 6);
+        map.replace("A", 1, 11);
+        map.remove("B", 10);
+        System.out.println(map);
+        System.out.println(map.get("A"));
+        System.out.println(map.getOrDefault("key",-1));
+        System.out.println(map.values());
+        System.out.println(map.keySet());
+        // 멀티 쓰레드 safe해야 한다면 ConcurrentHashMap을 사용해야 한다.
+        // HashMap : not synchronized
+        // Hashtable : synchronized
+        // ConcurrentHashMap : syncronized + hish concurrency
+    }
+
+    public static void method1(Map<String, Integer> map) {
+
+    }
+}
