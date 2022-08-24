3b9993f74ae6e952b6cf56c4965db705ee4a1c87
jaewoopk <jaewoopk000@naver.com>
2022-08-24 20:42:09 +0900

    Day 6 Lecture test 3_1 failed

--- Lecture3_test.java
+++ Lecture3_test.java
@@ -1,30 +1,40 @@
 import java.util.*;
 
 public class Lecture3_test {
-    static Map<Integer[], Integer> pokemon = new HashMap<>();
-    
+    static Map<int [], Integer> pokemon = new HashMap<>();
+    static int[] array = new int[2];
     public static void main(String[] args) {
         int[] nums = {3,1,2,3};
-        dfs(nums, 0, new Integer[1]);
+        dfs(nums, 0, 0);
 
     }
-    public static void dfs(int[] nums, int idx, Integer[] arr) {
+    public static void dfs(int[] nums, int idx, int count) {
         if (idx == nums.length / 2) {
-            if (!pokemon.containsKey(arr))
+            if (!pokemon.containsKey(array))
+            {
+                int[] arr = new int[2];
+                arr = Arrays.copyOf(array, 2);
                 pokemon.put(arr, 1);
+                System.out.println("이게 실행되나?");
+            }
             else {
+                int[] arr = new int[2];
+                arr = Arrays.copyOf(array, 2);
                 Integer tmp = pokemon.get(arr);
                 pokemon.replace(arr, ++tmp);
             }
-            System.out.println("arr = " + arr);
-            System.out.println(pokemon.keySet());
+            System.out.println("arr = " + array[0] + "  " + array[1]);
+            System.out.println(pokemon.values());
             return ;
+            // 아직은 내가 다루는 법을 잘 모르는 것 같으니 답을 보고 판단하자.
         }
-
-        for (int i = idx; i < nums.length - 1; i++) {
-            Integer[] array = new Integer[nums.length / 2];
-            array[i] = nums[i];
-            dfs(nums, ++idx, array);
+        for (int i = 0; i < nums.length; i++) {
+            if (count > i)  continue;
+            array[idx] = nums[i];
+            count = i;
+            dfs(nums, ++idx, ++count);
+            count--;
+            idx--;
         }
     }
 }
