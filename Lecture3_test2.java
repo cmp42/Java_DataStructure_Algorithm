a121eec9e229063e23d3369513a3ae6ed1c3bedf
jaewoopk <jaewoopk000@naver.com>
2022-08-25 16:11:43 +0900

    Day 7 Lecture 3 test2 I change algorithm but still failed at efficiency

--- Lecture3_test2.java
+++ Lecture3_test2.java
@@ -4,36 +4,23 @@ public class Lecture3_test2 {
     public static void main(String[] args) {
         String[] part = {"mislav", "stanko", "mislav", "ana"};
         String[] comp = {"stanko", "ana", "mislav"};
-        boolean check = false;
-        final List<String> arr = new ArrayList<>();
         
-        Collections.addAll(arr, comp);
-        String answer = "";
+        System.out.println("1234");
+        List<String> arr = new List<String>();
+        List<String> arr2 = new List<String>();
 
-        System.out.println(arr);
-        for (String str : part) {
-            for (String str2 : arr) {
-                if (str.equals(str2)) {
-                    check = true;
-                    break ;
-                }
-                check = false;
-            }
-            if (check) {
-                for (String str2 : arr) {
-                    System.out.println("str == " + str + " str2 == " + str2);
-                    if (str.equals(str2)){
-                        arr.remove(str2);
-                        break ;
-                    }
-                }
-            }
-            else {
-                answer = str;
+        Collections.addAll(arr, part);
+        Collections.addAll(arr2, comp);
+
+        for (int i = 0; i < arr.size(); i++) {
+            String str = arr.get(i);
+            if (arr2.contains(str)){
+                arr.remove(str);
+                arr2.remove(str);
+                i--;
             }
         }
-        System.out.println(answer);
-
+        System.out.println(arr);
     }
 
     public static String solution(String[] participant, String[] completion) {
