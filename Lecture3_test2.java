9e7185054745fddf2e3326893824ad85ff1367e4
jaewoopk <jaewoopk000@naver.com>
2022-08-25 17:14:01 +0900

    Day 7 Lecture test2 I repeated to change algorithm but still failed at efficiency

--- Lecture3_test2.java
+++ Lecture3_test2.java
@@ -1,26 +1,19 @@
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Lecture3_test2 {
     public static void main(String[] args) {
-        String[] part = {"mislav", "stanko", "mislav", "ana"};
-        String[] comp = {"stanko", "ana", "mislav"};
+        String[] participant = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion = {"stanko", "ana", "mislav"};
         
         System.out.println("1234");
-        List<String> arr = new List<String>();
-        List<String> arr2 = new List<String>();
-
-        Collections.addAll(arr, part);
-        Collections.addAll(arr2, comp);
-
-        for (int i = 0; i < arr.size(); i++) {
-            String str = arr.get(i);
-            if (arr2.contains(str)){
-                arr.remove(str);
-                arr2.remove(str);
-                i--;
-            }
+        List<String> part = new ArrayList(Arrays.asList(participant));
+        List<String> comp = Arrays.asList(completion);
+        
+        for (String str : comp) {
+            part.remove(str);
         }
-        System.out.println(arr);
+        System.out.println(part);
     }
 
     public static String solution(String[] participant, String[] completion) {
