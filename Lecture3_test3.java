f7575c3523834135f46bfc10218de881aca5fbc3
jaewoopk <jaewoopk000@naver.com>
2022-08-28 09:43:51 +0900

    Day 8 Lecture 3 test 3 about another solve algorithm to understand

--- Lecture3_test3.java
+++ Lecture3_test3.java
@@ -23,3 +23,20 @@ public class Lecture3_test3 {
 		System.out.println(result - 1);
     }
 }
+
+/*
+import java.util.*;
+import static java.util.stream.Collectors.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        //int answer = 0;
+        return Arrays.stream(clothes)
+            .collect( groupingBy( p -> p[1], mapping( p -> p[0], counting() ) ) )
+            .values()
+            .stream().collect( reducing(1L, (x, y) -> x * ( y + 1 ) ) )
+            .intValue() - 1;
+    }
+}
+// 이런 방법도 가능하다. 한줄로도 풀 수 있다는 뜻. 이런 부분을 많이 배워보자.
+ */
